34972d9e22756b85ab6a218bf3ae8b9bf1b0d2e7
vanessa <vanessa@genuitec.com>
2020-09-06 20:49:45 -0500

    adding script to project and test definition

---
+++ RunTests.bat
@@ -0,0 +1,5 @@
+set projectLocation=C:\personal\FirstTestNGProject
+cd %projectLocation%
+set classpath=%projectLocation%\bin;%projectLocation%\lib\*
+java org.testng.TestNG %projectLocation%\testng.xml
+pause
